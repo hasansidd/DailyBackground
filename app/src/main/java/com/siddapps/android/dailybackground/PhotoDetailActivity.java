d7a77e0271e1f0b335409743a0896616aba97542
hsidd89 <hasansidd@gmail.com>
2017-10-30 14:57:40 -0500

    Save file via MediaStore and use intent to crop with default home launcher

--- app/src/main/java/com/siddapps/android/dailybackground/PhotoDetailActivity.java
+++ app/src/main/java/com/siddapps/android/dailybackground/PhotoDetailActivity.java
@@ -1,14 +1,20 @@
 package com.siddapps.android.dailybackground;
 
+import android.Manifest;
 import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -19,8 +25,11 @@ import android.widget.LinearLayout;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import java.io.ByteArrayOutputStream;
+
 public class PhotoDetailActivity extends AppCompatActivity {
     public static final String TAG = "PhotoDetailActivity";
+    public static final int PERMISSION_CODE_WRITE_EXTERNAL_STORAGE = 1;
     private ImageView mImageView;
     private Button mButton;
     private LinearLayout mLinearLayout;
@@ -48,11 +57,7 @@ public class PhotoDetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mButton.setEnabled(false);
-                WallpaperManager wallpaperManager = WallpaperManager.getInstance(PhotoDetailActivity.this);
-                    Uri uri = Uri.parse(urlString);
-                    Intent i = wallpaperManager.getCropAndSetWallpaperIntent(uri);
-                    startActivity(i);
-                    Snackbar.make(mLinearLayout, "Wallpaper set!", Snackbar.LENGTH_SHORT).show();
+                setWallpaper();
             }
         });
 
@@ -79,5 +84,46 @@ public class PhotoDetailActivity extends AppCompatActivity {
         Picasso.with(PhotoDetailActivity.this)
                 .load(urlString)
                 .into(mTarget);
+    }
+
+
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_CODE_WRITE_EXTERNAL_STORAGE);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CODE_WRITE_EXTERNAL_STORAGE:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    setWallpaper();
+                } else {
+                    mButton.setEnabled(true);
+                }
+                break;
+            default:
+                mButton.setEnabled(true);
+        }
+    }
+
+    private void setWallpaper() {
+        if (checkPermission()) {
+            WallpaperManager wallpaperManager = WallpaperManager.getInstance(PhotoDetailActivity.this);
+            Intent i = wallpaperManager.getCropAndSetWallpaperIntent(getImageUri(mBitmap, PhotoDetailActivity.this));
+            startActivity(i);
+            Snackbar.make(mLinearLayout, "Wallpaper set!", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    private Uri getImageUri(Bitmap bitmap, Context context) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
+        Log.e(TAG, path);
+        return Uri.parse(path);
     }
 }
